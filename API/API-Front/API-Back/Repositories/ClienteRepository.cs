9df3fd170c848fe13f52c116baca42beb500b846
tomiloco05 <tomilocaso1@gmail.com>
2024-11-12 13:54:17 -0300

    UsuarioRepository

---
+++ API/API-Front/API-Back/Repositories/ClienteRepository.cs
@@ -0,0 +1,34 @@
+﻿using API_Back.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace API_Back.Repositories
+{
+    public class ClienteRepository : IClienteRepository
+    {
+        private CineDBContext _context;
+
+        public ClienteRepository(CineDBContext context)
+        {
+            _context = context;
+        }
+        public Usuario? GetByMail(string Email)
+        {
+            return _context.Usuarios.Where(U => U.EMail == Email).FirstOrDefault();
+        }
+
+        public Usuario? Login(string Email, string password)
+        {
+            return _context.Usuarios.Where(U => U.EMail == Email && U.Contrasenia == password).FirstOrDefault();
+        }
+
+        public bool Register(Usuario oUsuario)
+        {
+            _context.Usuarios.Add(oUsuario);
+            return _context.SaveChanges() == 1;
+        }
+    }
+}
---
+++ API/API-Front/API-Back/Repositories/IClienteRepository.cs
@@ -0,0 +1,17 @@
+﻿using API_Back.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace API_Back.Repositories
+{
+    public interface IClienteRepository
+    {
+        Usuario? GetByMail(string Email);
+        Usuario? Login(string Email, string password);
+        bool Register(Usuario usuario);
+
+    }
+}
