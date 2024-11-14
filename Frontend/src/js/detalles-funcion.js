13506acc42180c160e4411ea39b2ba48c5efbf6d
tomiloco05 <tomilocaso1@gmail.com>
2024-11-14 13:33:14 -0300

    Parte 1 Asientos Funciones

--- Frontend/src/js/detalles-funcion.js
+++ Frontend/src/js/detalles-funcion.js
@@ -9,22 +9,48 @@ const cargarAsientos = async function () {
         return
     }
 
-    const matrizAsientos = await fetch(`http://localhost:5141/api/Sala?idSala=${idSala}&${idReproduccion}`)
+    const matrizAsientos = await fetch(`http://localhost:5141/api/Sala?idSala=${idSala}&idReproduccion=${idReproduccion}`)
     .then (response => response.json())
     
     for (let fila of matrizAsientos) {
         const filaAsientos = document.createElement("div")
-        for (let columna of fila) {
+        for (let columna of fila) { 
             const asiento = document.createElement("input")
 
             // asiento.setAttribute("name")
             asiento.type = "checkbox"
-            asiento.textContent = columna.nombre
+            asiento.textContent = columna.name
+            asiento.name = columna.id
+            asiento.checked = columna.ocupado
+            if (columna.ocupado)
+            {
+                asiento.disabled=true
+            }
+            else
+            {
+                const TextoLista = document.createElement("span")
+                TextoLista.textContent=asiento.name
+                TextoLista.id="TxT-"+asiento.name
+                asiento.appendChild(TextoLista)
+                asiento.addEventListener('change',(event)=>
+                {
+                    if(event.currentTarget.checked)
+                    {
+                        $("#B").appendChild(event.currentTarget.firstChild)
+                    }
+                    else
+                    {
+                        event.currentTarget.appendChild($("#TxT-"+event.currentTarget.name))
+                    }
+                })
+            }
+
             filaAsientos.appendChild(asiento)
         }
 
-        document.body.appendChild(filaAsientos)
+        document.getElementById("A").appendChild(filaAsientos)
     }
 }
 
 document.addEventListener("DOMContentLoaded", cargarAsientos)
+
