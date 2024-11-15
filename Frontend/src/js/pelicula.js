8506b149cda9aabc8c7d45c1e821352f45063032
tomiloco05 <tomilocaso1@gmail.com>
2024-11-15 01:47:12 -0300

    Informacion sobre las peliculas

--- Frontend/src/js/pelicula.js
+++ Frontend/src/js/pelicula.js
@@ -1,13 +1,23 @@
 const cargarPelicula = async function () {
     const urlParams = new URLSearchParams(window.location.search);
 
-    const id = urlParams.get('id'); 
+    const id = urlParams.get('id');
+    
     return fetch(`http://localhost:5141/api/Pelicula/${id}`)
     .then(response => response.json())
     .then(pelicula => {
         $(".titulo").textContent = pelicula.nombre
         $(".imagen").src = pelicula.urlPortada
         $(".descripcion").textContent = pelicula.sinopsis
+        $(".duracion").textContent = "Duracion: "+pelicula.duracionMinutos+" Minutos"
+        $(".fecha").textContent = "Fecha de estreno: "+pelicula.fechaEstreno
+       let IdGenero = pelicula.idGenero
+       return fetch(`http://localhost:5141/api/genero/${IdGenero}`)
+       .then(response => response.json())
+       .then(Genero => {
+            $(".genero").textContent = Genero.nombre
+       })
+   
     })
 }
 
